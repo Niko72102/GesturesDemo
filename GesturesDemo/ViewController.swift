72ca5ed2951e39dfbde931421e276f22805d58b6
IMANOL MUNOZ <rl200156@rlas-macair-29.rlas-116.org>
2018-11-08 11:14:33 -0600

    Initial Commit

---
+++ GesturesDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GesturesDemo
+//
+//  Created by IMANOL MUNOZ on 11/8/18.
+//  Copyright © 2018 IMANOL MUNOZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
